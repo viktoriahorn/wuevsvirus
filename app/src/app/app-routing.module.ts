5c131d62733303f64ac463a26e8c33b9ff5bfbd6
Gregor <gregor@pop-os.localdomain>
2020-10-24 12:34:07 +0200

    seperate views, add dirs for widgets and service classes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./views/tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
